bbc06116ccb3dff168d48c62b71e2b9641665260
Muhammad Umar Iqbal <umar.iqbal111444@gmail.com>
2024-02-05 22:07:06 +0500

    first commit

--- put_data.py
+++ put_data.py
@@ -2,18 +2,17 @@ import mysql.connector
 from mysql.connector import Error
 import json
 
-# Function to determine the MySQL data type based on Python type
 def get_data_type(value):
     if isinstance(value, int):
         return 'INT'
     elif isinstance(value, float):
-        return 'DECIMAL(9, 6)'  # Adjust precision and scale as needed for float values
+        return 'DECIMAL(9, 6)' 
     elif isinstance(value, str):
-        return 'VARCHAR(255)'  # Adjust length as needed for string values
+        return 'VARCHAR(255)'  
     else:
-        return 'VARCHAR(255)'  # Default to VARCHAR for unknown types
+        return 'VARCHAR(255)' 
 
-# Function to flatten nested dictionaries
+
 def flatten_dict(d, parent_key='', sep='_'):
     items = []
     for k, v in d.items():
@@ -24,24 +23,24 @@ def flatten_dict(d, parent_key='', sep='_'):
             items.append((new_key, v))
     return dict(items)
 
-# Replace these values with your MySQL server details
+
 host = 'localhost'
 user = 'root'
 password = ''
 database = 'weatherapi'
-table_name = 'hello'
+table_name = 'data'
 
-# JSON file path
+
 json_file_path = 'C:/Users/DELLL/Desktop/h/api_data.json'
 
-# Read JSON data from file
+
 with open(json_file_path, 'r') as json_file:
     json_data = json.load(json_file)
 
-# Flatten the JSON data
+
 flat_json_data = flatten_dict(json_data)
 
-# Connect to MySQL
+
 try:
     connection = mysql.connector.connect(
         host=host,
@@ -53,8 +52,7 @@ try:
     if connection.is_connected():
         cursor = connection.cursor()
 
-        # Create table with the same structure as 'hello'
-        create_table_query = f"CREATE TABLE IF NOT EXISTS `hello` ("
+        create_table_query = f"CREATE TABLE IF NOT EXISTS `data` ("
         create_table_query += "`id` INT AUTO_INCREMENT PRIMARY KEY, "
         for key, value in flat_json_data.items():
             create_table_query += f"`{key}` {get_data_type(value)}, "
@@ -63,8 +61,8 @@ try:
         print(f"SQL Query: {create_table_query}")
         cursor.execute(create_table_query)
 
-        # Insert values into the table
-        insert_query = f"INSERT INTO `hello` ({', '.join(flat_json_data.keys())}) VALUES ({', '.join(['%s'] * len(flat_json_data))})"
+        
+        insert_query = f"INSERT INTO `data` ({', '.join(flat_json_data.keys())}) VALUES ({', '.join(['%s'] * len(flat_json_data))})"
         values = tuple(flat_json_data[key] if not isinstance(flat_json_data[key], list) else json.dumps(flat_json_data[key]) for key in flat_json_data)
 
         cursor.execute(insert_query, values)
